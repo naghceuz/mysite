843575c4743f3a9e779539a5218eacb7cbbabd94
naghceuz <guanchenzhang@gmail.com>
2014-11-13 22:17:00 -0500

    Fourth Commit
    Customize the poll

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from polls.models import Choice, Question
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+
+
+
 
 # Register your models here.
+admin.site.register(Question, QuestionAdmin)

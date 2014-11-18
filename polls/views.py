915816a7e9064444fd45fffbdeb2343468be12a4
naghceuz <guanchenzhang@gmail.com>
2014-11-18 04:43:29 -0500

    fifth commit
    apply css effect

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,41 @@
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+
 from django.shortcuts import render
 
-# Create your views here.
+from polls.models import Question
+
+
+# directly talk to database
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([p.question_text for p in latest_question_list])
+#     return HttpResponse(output)
+
+# use a template
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = RequestContext(request, {
+#         'latest_question_list': latest_question_list,
+#     })
+#     return HttpResponse(template.render(context))
+
+
+def index(request):
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+# Leave the rest of the views (detail, results, vote) unchanged
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
